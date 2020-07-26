8d92faa65aba0a99b3c38203c8cafddacd0ee17a
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2020-07-26 09:01:39 -0400

    chore: created appointment routes

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { parseISO } from 'date-fns';
+import AppointmentRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../service/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.get('/', async (req, res) => {
+  const appointmentsRepository = getCustomRepository(AppointmentRepository);
+  const appointments = await appointmentsRepository.find();
+
+  return res.json(appointments);
+});
+
+appointmentsRouter.post('/', async (req, res) => {
+  const { provider, date } = req.body;
+  const parsedDate = parseISO(date);
+  const createAppointment = new CreateAppointmentService();
+
+  const appointment = await createAppointment.execute({
+    date: parsedDate,
+    provider,
+  });
+
+  return res.json(appointment);
+});
+
+export default appointmentsRouter;
